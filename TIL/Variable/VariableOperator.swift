4eacbf7bdc18b4e2f73b20a7b1d4fe6f950823b0
Sunjoo IM <tjss1580@gmail.com>
2023-05-15 21:39:29 +0900

    Add: Operators

---
+++ TIL/Variable/VariableOperator.swift
@@ -0,0 +1,40 @@
+
+import SwiftUI
+
+struct VariableOperator: View {
+    var body: some View {
+        var age = 25
+        var anyNumber = 4
+        var anyNumber2 = 15
+        var anyNumber3 = 4
+        
+        VStack {
+            VStack {
+                // 사칙연산자
+                Text("\(age + anyNumber)")
+                Text("\(age - anyNumber)")
+                Text("\(age * anyNumber)")
+                Text("\(age / anyNumber)")
+                Text("\(age % anyNumber)")
+            }
+            .padding()
+            
+            VStack {
+                // 비교연산자
+                Text("\(anyNumber < anyNumber2)".description)
+                Text("\(anyNumber > anyNumber2)".description)
+                Text("\(anyNumber <= anyNumber2)".description)
+                Text("\(anyNumber >= anyNumber2)".description)
+                Text("\(anyNumber == anyNumber3)".description)
+                Text("\(anyNumber != anyNumber3)".description)
+            }
+        }
+        .font(.title2)
+    }
+}
+
+struct VariableOperator_Previews: PreviewProvider {
+    static var previews: some View {
+        VariableOperator()
+    }
+}
