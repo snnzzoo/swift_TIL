ac8ce94dbcfabc695fbef5c8164d14e52e906c1f
Sunjoo IM <tjss1580@gmail.com>
2023-05-15 20:18:22 +0900

    Initial Commit

---
+++ TIL/TILApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILApp.swift
+//  TIL
+//
+//  Created by Sunjoo IM on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct TILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
