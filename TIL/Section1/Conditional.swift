864ba5fdb398f9d9d2b6967054f5f5b7b9c5fe9e
Sunjoo IM <tjss1580@gmail.com>
2023-05-17 19:37:37 +0900

    Add: 조건문

---
+++ TIL/Section1/Conditional.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct Conditional: View {
+    var body: some View {
+        
+        var randomNumber: Int = 3
+        
+        if (randomNumber % 2) == 0 {
+            Text("짝수")
+        } else {
+            Text("홀수")
+        }
+        
+        //        if <#condition#> { // if 조건이 참이면
+        //            <#code#>
+        //        } else { // 거짓이면
+        //
+        //        }
+    }
+}
+
+struct Conditional_Previews: PreviewProvider {
+    static var previews: some View {
+        Conditional()
+    }
+}
